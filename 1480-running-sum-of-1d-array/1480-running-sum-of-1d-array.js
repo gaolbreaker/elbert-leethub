c73962760f747f9091916cbdb9d2b73b432a8a6e
Elbert Chan <elb.chan@gmail.com>
2022-06-18 15:14:23 -0700

    Time: 70 ms (82.00%), Space: 42.5 MB (35.45%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,13 +3,12 @@
  * @return {number[]}
  */
 const runningSum = function(nums) {
-    const arr = [];
     for (let i = 0; i < nums.length; i++) {
         if (i === 0) {
-            arr[i] = nums[i];
+            nums[i] = nums[i];
         } else {
-            arr[i] = arr[i - 1] + nums[i];
+            nums[i] = nums[i - 1] + nums[i];
         }
     }
-    return arr;
+    return nums;
 };
