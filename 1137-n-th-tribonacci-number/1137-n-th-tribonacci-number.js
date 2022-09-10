a20a3881ed03125525ca3928d0269c3cedba5139
Elbert Chan <elb.chan@gmail.com>
2022-09-10 14:16:58 -0700

    Time: 103 ms (19.45%), Space: 42.1 MB (15.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -16,16 +16,32 @@ var tribonacci = function(n) {
   */
   
   // seed
-    let t_0 = 0;
-    let t_1 = 1;
-    let t_2 = 1;
-    let t_n = 2;
-    const table = [0, 1, 1]
+    // let t_0 = 0;
+    // let t_1 = 1;
+    // let t_2 = 1;
+    // let t_n = 2;
+    // const table = [0, 1, 1]
   
-  // handle edge cases
-    if (n === 0) return 0;
-    if (n === 1) return 1;
-    if (n === 2) return 1;
+  // closure variable 
+  const store = {};
+  store[0] = 0;
+  store[1] = 1;
+  store[2] = 2;
+  
+  let helper = (input) => {
+  
+    // handle edge cases
+    if (input === 0) return 0;
+    if (input === 1) return 1;
+    if (input === 2) return 1;
+    if (store[input]) return store[input];
+    
+    const result = helper(input - 3) + helper(input - 2) + helper(input - 1);
+    store[input] = result;
+    return result;
+    
+  
+  }
     
   
   // loop
@@ -36,11 +52,11 @@ var tribonacci = function(n) {
 //       t_1 = t_2;
 //       t_2 = t_n;
 //     }
-    for (let i = 3; i <= n; i++) {
-      table[i] = (table[i - 3] + table[i - 2] + table[i - 1]);
-    }
+    // for (let i = 3; i <= n; i++) {
+    //   table[i] = (table[i - 3] + table[i - 2] + table[i - 1]);
+    // }
   
-  return table[n];
+  return helper(n);
   
     
 };
