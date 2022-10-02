9287aa8b50866471a547e2aee29a14403bde3286
Elbert Chan <elb.chan@gmail.com>
2022-10-02 13:30:52 -0700

    Time: 116 ms (37.23%), Space: 45.2 MB (52.97%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -24,5 +24,7 @@ var maxDepth = function(root) {
       
   */
   if (root === null) return 0;
-  else return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+  // BRB getting somethign to eat too
+  // problem is solved
 };
