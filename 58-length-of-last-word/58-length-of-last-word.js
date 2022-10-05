6505739f2542514e624304999826ef89936f1527
Elbert Chan <elb.chan@gmail.com>
2022-10-05 13:54:33 -0700

    Time: 113 ms (11.41%), Space: 43.9 MB (5.23%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -6,17 +6,19 @@ var lengthOfLastWord = function(s) {
   let lastWord;
   let currWord = '';
   
+  var s = s.trim();
+  
   for (let i = 0; i < s.length; i++) {
     if (s[i] !== ' ') {
       currWord += s[i];
-      if (i === s.length - 1) lastWord = currWord;
+      if (i === s.length - 1) return currWord.length;
     } else {
-      if (currWord.length > 0) {
-        lastWord = currWord;
-      }
+      // if (currWord.length > 0) {
+      //   lastWord = currWord;
+      // }
       currWord = '';
     }
   }
-  return lastWord.length;
+  // return lastWord.length;
   
 };
