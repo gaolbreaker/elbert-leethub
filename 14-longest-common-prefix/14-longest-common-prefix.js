81907624c196c329982ffc1a6027120d8e86c059
Elbert Chan <elb.chan@gmail.com>
2022-10-05 15:31:24 -0700

    Time: 67 ms (94.75%), Space: 42.8 MB (40.64%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  /*
+    I: array of strings
+    O: string representing "longest common prefix string"
+    C: 1 <= strs.length <= 200
+       0 <= strs[i].length <= 200
+       strs[i] consists of only lowercase English letters
+    E: strs.length === 1, return the entire string
+    P: 
+    
+       Example 1
+       ["flower", "flow", "flight"]
+       
+       
+  */
+  
+  // edge case
+  if (strs.length === 1) {
+    return strs[0];
+  }
+  
+  let result = '';
+  
+  const minStrLength = Math.min(...strs.map((e) => e.length));
+  for (let i = 0; i <= minStrLength; i++) {
+    let commonality = strs.map((e) => e[i]).reduce((a, b) => a === b ? b : false);
+    if (commonality) result += commonality;
+    else break;
+  }
+  
+  return result;
+  
+  
+};
