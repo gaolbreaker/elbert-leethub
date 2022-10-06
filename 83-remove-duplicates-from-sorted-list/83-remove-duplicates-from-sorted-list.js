c67b36770de71c0864642109423f31ad9362ac4d
Elbert Chan <elb.chan@gmail.com>
2022-10-06 02:12:21 -0700

    Time: 91 ms (78.83%), Space: 44.4 MB (42.04%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  var curr = head;
+  var prev = null;
+  while(curr) {
+    if (curr.val === prev?.val) {
+      prev.next = curr.next;
+      curr = curr.next;
+    } else {
+      prev = curr;
+      curr = curr.next;
+    }
+  }
+  return head;
+};
