f83bf4ef6fbfcc8333851f37250c08e789efc6bc
Elbert Chan <elb.chan@gmail.com>
2022-10-06 02:13:13 -0700

    Time: 119 ms (43.43%), Space: 44.1 MB (66.33%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.js
@@ -15,11 +15,10 @@ var deleteDuplicates = function(head) {
   while(curr) {
     if (curr.val === prev?.val) {
       prev.next = curr.next;
-      curr = curr.next;
     } else {
       prev = curr;
-      curr = curr.next;
     }
+    curr = curr.next;
   }
   return head;
 };
