42c92aa99595c65a8bc2e4f591212f081a4f8771
Elbert Chan <elb.chan@gmail.com>
2022-10-07 16:11:39 -0700

    Time: 78 ms (74.32%), Space: 41.9 MB (55.00%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+  
+  /*
+             b  b
+    1. 2  3. 4. 5
+    l     m     r
+             lm r
+            lmr
+         b   
+    1.   2.   3
+    l    m    r
+   lmr
+  */
+    return function(n) {
+      let left = 1;
+      let right = n; 
+      
+      while(left <= right) {
+        mid = Math.floor((left + right) / 2);
+        midResult = isBadVersion(mid);
+        if (midResult) { // found a bad ver
+          right = mid - 1;
+        } else { // DIDN'T find a bad ver, so it's to the right
+          if (left === right) {
+            return mid + 1;
+          } else left = mid + 1;
+        }
+      }
+      return mid;
+    };
+};
