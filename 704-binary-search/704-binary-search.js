eb5867ae40e252ab9ac3b96a975277dfb0d32c17
Elbert Chan <elb.chan@gmail.com>
2022-10-08 13:41:38 -0700

    Time: 99 ms (59.00%), Space: 45.3 MB (31.01%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -17,13 +17,13 @@ var search = function(nums, target) {
   
   while(left <= right) {
     mid = left + Math.floor((right - left) / 2);
-    if (nums[mid] === target) {
-      return mid;
-    }
+    if (nums[mid] === target) return mid;
     if (target < nums[mid]) right = mid - 1;
     if (target > nums[mid]) left = mid + 1;
   }
   
   return -1;
+  
+  // Don't conflate the index with the element of the array AT that index!!!
     
 };
