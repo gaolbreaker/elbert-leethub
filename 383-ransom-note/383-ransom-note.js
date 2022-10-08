9db401261697280bc3dfc05843fd825b4e564bc3
Elbert Chan <elb.chan@gmail.com>
2022-10-07 17:08:56 -0700

    Time: 89 ms (93.82%), Space: 46.1 MB (39.67%) - LeetHub

--- 383-ransom-note/383-ransom-note.js
+++ 383-ransom-note/383-ransom-note.js
@@ -6,16 +6,14 @@
 var canConstruct = function(ransomNote, magazine) {
   var ransom = ransomNote.split('');
   var mag = magazine.split('');
-  var letter;
   var idx;
   
   while (ransom.length > 0) {
-    letter = ransom.pop();
-    idx = mag.indexOf(letter);
-    if (idx > -1) {
-      mag.splice(idx, 1);
-    } else {
+    idx = mag.indexOf(ransom.pop());
+    if (idx === -1) {
       return false;
+    } else {
+      mag.splice(idx, 1); // switched order for time complexity, avoiding the splice if not needed
     }
   }
   return true;
