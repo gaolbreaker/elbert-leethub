d369c57313bd080bd1303d35addd431ef4599a1c
Elbert Chan <elb.chan@gmail.com>
2022-10-12 09:35:41 -0700

    Time: 100 ms (53.17%), Space: 44.5 MB (9.76%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -5,23 +5,17 @@
  */
 var backspaceCompare = function(s, t) {
   let i = 0;
-  let si;
-  let ti;
   let sTyped = [];
   let tTyped = [];
   while (s[i] !== undefined || t[i] !== undefined) {
-    si = s[i];
-    ti = t[i];
     
-    if (si === '#') sTyped.pop();
-    else if (si !== undefined) sTyped.push(si);
-    if (ti === '#') tTyped.pop();
-    else if (ti !== undefined) tTyped.push(ti);
+    if (s[i] === '#') sTyped.pop();
+    else if (s[i] !== undefined) sTyped.push(s[i]);
+    if (t[i] === '#') tTyped.pop();
+    else if (t[i] !== undefined) tTyped.push(t[i]);
     
     i++;
   }
-  console.log(sTyped);
-  console.log(tTyped);
   
   if (sTyped.join('') === tTyped.join('')) return true;
   else return false;
