1be91bd334f9e9ae7831121054cbd3c919c565a0
Elbert Chan <elb.chan@gmail.com>
2022-10-12 16:11:51 -0700

    Time: 58 ms (99.70%), Space: 43.2 MB (81.51%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -9,12 +9,13 @@ var singleNumber = function(nums) {
 //   let popped;
   
 //   while (nums.length > 0) {
-//     popped = nums.pop();
+//     popped = nums.pop(); // I am popping from the array so while extra space is used, the array also shrinks, so would it be correct to say it's O(1) space complexity because it evens out?
 //     if (!hash[popped]) hash[popped] = 1;
 //     else delete hash[popped];
 //   }
 //   return Object.keys(hash)[0];
   
+// bitwise operator solution
   return nums.reduce((a, b) => a ^ b);
     
 };
