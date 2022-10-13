f969afd33106e80d0e7ae97d7a0c248f5f833a33
Elbert Chan <elb.chan@gmail.com>
2022-10-12 17:18:54 -0700

    Time: 237 ms (52.86%), Space: 84.2 MB (34.94%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -18,23 +18,33 @@ var isPalindrome = function(head) {
 
     const arr = [];
     let curr = head;
+    let prev = null;
     while (curr) {
-      arr.push(curr.val);
+      // arr.push(curr.val);
+      curr.prev = prev;
+      prev = curr;
       curr = curr.next;
     }
-    // if (JSON.stringify(arr.slice(0, Math.floor(arr.length / 2))) === JSON.stringify(arr.slice(Math.round(arr.length / 2)).reverse())) {
-    //   return true;
-    // } else return false;
-    let i = 0;
-    let j = arr.length - 1;
-    while (i <= j) {
-      if (arr[i] !== arr[j]) return false;
-      else {
-        i++;
-        j--;
-      }
-    } 
+    const tail = prev;
+    let left = head;
+    let right = tail;
+    while (left !== right) {
+      if (left.val !== right.val) return false;
+      left = left.next;
+      right = right.prev;
+    }
     return true;
+
+    // let i = 0;
+    // let j = arr.length - 1;
+    // while (i <= j) {
+    //   if (arr[i] !== arr[j]) return false;
+    //   else {
+    //     i++;
+    //     j--;
+    //   }
+    // } 
+    // return true;
   
   
 };
