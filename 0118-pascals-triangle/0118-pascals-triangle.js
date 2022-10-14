2b6c075204b33a12691d905359787e3c34d5bdab
Elbert Chan <elb.chan@gmail.com>
2022-10-14 02:11:12 -0700

    Time: 93 ms (50.50%), Space: 41.9 MB (58.01%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,13 +3,13 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-  const result = [];
+  const res = [];
   for (let i = 1; i <= numRows; i++) { // i is the row number
-    result[i - 1] = [];
+    res[i - 1] = [];
     for (let j = 1; j <= i; j++) { // j is the column number, 1-indexed
-      if (j === 1 || i === j) result[i -1][j - 1] = 1; // leftmost and rightmost elements of a row
-      else result[i - 1][j - 1] = (result[i - 2][j - 2] + result[i - 2][j - 1]);      
+      if (j === 1 || i === j) res[i -1][j - 1] = 1; // leftmost and rightmost elements of a row
+      else res[i - 1][j - 1] = (res[i - 2][j - 2] + res[i - 2][j - 1]);      
     }
   }
-  return result;
+  return res;
 };
