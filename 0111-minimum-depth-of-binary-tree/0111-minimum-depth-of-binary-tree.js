db22896421c4fab284443f7e2dccfbbc0d3f052d
Elbert Chan <elb.chan@gmail.com>
2022-10-14 00:41:43 -0700

    Time: 276 ms (82.60%), Space: 99.2 MB (30.98%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -13,11 +13,9 @@
 var minDepth = function(root) {
   return (
     root === null ? 
-      0 : 
+      0 : // takes care of null nodes (e.g., null root)
       root.left && root.right ? 
-        1 + Math.min(minDepth(root.left), minDepth(root.right)) :
-        !!!root.left && !!!root.right ?
-          1 :
-          1 + (root.left ? minDepth(root.left) : minDepth(root.right))
+        1 + Math.min(minDepth(root.left), minDepth(root.right)) : // takes care of 2-child nodes
+        1 + (root.left ? minDepth(root.left) : minDepth(root.right)) // takes care of 1-child or 0-child nodes
   );  
 };
