d42f9a8ff6f5af50bf5b222d1c0e56409e9cd21c
Elbert Chan <elb.chan@gmail.com>
2022-10-19 16:12:19 -0700

    Time: 103 ms (87.36%), Space: 49.3 MB (92.33%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -13,33 +13,59 @@
  */
 var getIntersectionNode = function(headA, headB) {
   
-  // O(m + n) space complexity
-  
-  if (headA === headB) return headA;
-  
-  const arrA = [];
-  const arrB = [];
-  
   let curr = headA;
-  let result = null;
+  let curr2 = headA;
   
+  // set a flag on each node in linked list 1
   while (curr) {
-    arrA.push(curr);
+    curr.visited = true;
     curr = curr.next;
   }
-  
+  // reset curr
   curr = headB;
   
   while (curr) {
-    arrB.push(curr);
-    curr = curr.next;
-  }
-
-  while (arrA[arrA.length - 1] === arrB[arrB.length - 1]) {
-    result = arrA.pop();
-    arrB.pop();
+    if (curr.visited) {
+      // revert the first linkedList to original state
+      while (curr2) {
+        delete curr2.visited;
+        curr2 = curr2.next;
+      }
+      // return the node that was "visited" before! that's the intersection point!
+      return curr;
+    } else {
+      curr = curr.next;
+    }
   }
   
-  return result;
+  // O(m + n) space complexity
+  // O(m + n) time complexity
+  
+//   if (headA === headB) return headA;
+  
+//   const arrA = [];
+//   const arrB = [];
+  
+//   let curr = headA;
+//   let result = null;
+  
+//   while (curr) {
+//     arrA.push(curr);
+//     curr = curr.next;
+//   }
+  
+//   curr = headB;
+  
+//   while (curr) {
+//     arrB.push(curr);
+//     curr = curr.next;
+//   }
+
+//   while (arrA[arrA.length - 1] === arrB[arrB.length - 1]) {
+//     result = arrA.pop();
+//     arrB.pop();
+//   }
+  
+//   return result;
   
 };
