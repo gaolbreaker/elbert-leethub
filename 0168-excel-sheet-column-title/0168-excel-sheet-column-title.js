123ef91b282b95d3cc459d38108aea8b7d9c6eba
Elbert Chan <elb.chan@gmail.com>
2022-10-21 15:03:13 -0700

    Time: 107 ms (15.90%), Space: 41.8 MB (66.72%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+  const arr = ['Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const res = [];
+  let power = 1;
+  while (columnNumber > 0) {
+    res.push(arr[columnNumber % 26]);
+    if (columnNumber % 26 === 0) columnNumber -= 26;
+    else columnNumber -= columnNumber % 26;
+    columnNumber /= 26;
+  }
+  
+  return res.reverse().join('');
+  
+};
