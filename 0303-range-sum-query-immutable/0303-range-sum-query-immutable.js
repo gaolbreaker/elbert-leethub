48c45ea4638f1d3b396fa25d482cc4ad4e71cacf
Elbert Chan <elb.chan@gmail.com>
2022-10-31 17:21:26 -0700

    Time: 327 ms (20.82%), Space: 48.8 MB (76.51%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -11,18 +11,10 @@ var NumArray = function(nums) {
  * @return {number}
  */
 NumArray.prototype.sumRange = function(left, right) {
-  
   let i = 0;  
   let sum = 0;
   while (i <= right) {
-    if (i < left) {
-      i++;
-      continue;
-    }
-    // i >= left
-    // console.log('left: ' + left + ' | ' + ' right: ' + right + ' i: ' + i + ' sum: ' + sum);
-    // console.log(this.nums[i]);
-    sum += this.nums[i];
+    if (i >= left) sum += this.nums[i];    
     i++;
   }
   return sum;
