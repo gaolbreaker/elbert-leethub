dfe901270bc2826da7270ed2a4d3b5d96591536e
Elbert Chan <elb.chan@gmail.com>
2022-11-08 20:16:41 -0800

    Time: 93 ms (60.82%), Space: 42.3 MB (24.10%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.js
+++ 2235-add-two-integers/2235-add-two-integers.js
@@ -4,5 +4,37 @@
  * @return {number}
  */
 var sum = function(num1, num2) {
-    return num1 + num2;
+  
+  /*
+  
+    step through sum(13, 11):
+    
+      1101 = 13
+      1011 = 11
+      xor =                0110
+      carry = 1001 << 1 = 10010
+      &                   00010
+       0110
+      10010
+      xor =                10100
+      carry = 00010 << 1 = 00100
+      &                    00100
+      10100
+      00100
+      xor =                10000
+      carry = 00100 << 1 = 01000
+      &                    00000
+      ^                    11000 = 24
+    
+  */
+  var xor = num1 ^ num2;
+  // console.log(`xor: ${xor}`);
+  var carry = (num1 & num2) << 1;
+  // console.log(`carry: ${carry}`)
+  if (xor & carry) {
+    return sum(xor, carry);
+  } else {
+    return xor ^ carry;
+  }
+  
 };
