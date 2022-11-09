a8bf71aae3e8a43f9ac8d6e07e444d3d26a3fa38
Elbert Chan <elb.chan@gmail.com>
2022-11-09 13:39:09 -0800

    Time: 77 ms (90.45%), Space: 43.9 MB (91.77%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -7,7 +7,16 @@ var shuffle = function(nums, n) {
   /*
     [0,    1,    2,    3] n = 2
      i          i+n
+     
+    [0, 1, 2, 3, 4, 5] n = 3, grab i + n first. and stick it at i + 1. 
+    [0, 3, 2, n, 4, 5] swapped, out = 1
+    [0, 3, 1, n, 4, 5] swapped, out = 2
+    [0, 3, 1, 4, n, 5] swapped, out = 2
+    [0, 3, 1, 4, 2, 5] swapped, out = 2
+    [0, 3, 1, 4, 2, 5]
   */
+  
+  // extra memory version:
   const res = [];
   
   for (let i = 0; i < n; i++) {
