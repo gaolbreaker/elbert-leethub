7cf8a8577d47ddf0115d7d60f43732e55d42daec
Elbert Chan <elb.chan@gmail.com>
2022-11-11 23:05:59 -0800

    Time: 77 ms (78.09%), Space: 42.1 MB (21.05%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,11 +3,13 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-  let res = '';
-  for (let i = 0; i < address.length; i++) {
-    if (address[i] === '.') res += '[.]';
-    else res += address[i];
-  }
-  return res;
+  return address.split('').map(e => e === '.' ? '[.]' : e).join('');
+  
+  // let res = '';
+  // for (let i = 0; i < address.length; i++) {
+  //   if (address[i] === '.') res += '[.]';
+  //   else res += address[i];
+  // }
+  // return res;
   
 };
