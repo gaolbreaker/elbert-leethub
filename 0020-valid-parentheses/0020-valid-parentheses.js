2db10a7738ed4187db92ba21979551a6f2bc8016
Elbert Chan <elb.chan@gmail.com>
2022-11-15 10:55:36 -0800

    Time: 134 ms (11.42%), Space: 42.5 MB (54.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,51 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    /*
+      I: a string containing characters for parentheses, squiggly brackets, and square brackets
+      O: boolean, true if valid
+      C: 1 <= s.length <= 10^4
+         s consists of parentheses only '()[]{}''
+      E: ???
+      
+      Example 1: '()' 
+        left parenthesis in position 1 and there is a closing parenthesis afterwards somewhere, so returns true
+      Example 2: '()[]{}'
+        each left parenthesis eventually is closed by a corresponding right parenthesis
+      Example 3: '(]'
+        left round parenthesis is not closed by a right round parenthesis
+        so returns false
+        
+      Algorithm ideas:
+        -Count # of left round parentheses. Should correspond with number of right round parentheses
+        Example 2: '(][){}'
+      Pseudocode:
+        declare an array for our stack
+        iterate through the string
+          if left parenthesis is found, push the character to the stack
+          if right parenthesis found, assess whether stack contains corresponding left parenthesis, if not, return false
+            if yes, pop off the stack
+      
+    */
+    // if (s.length === 1) return false;
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+       if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+         stack.push(s[i])
+       }
+       if (s[i] === ')') {
+         if (stack.pop() !== '(') return false;
+       }
+       if (s[i] === ']') {
+         if (stack.pop() !== '[') return false;
+       }
+       if (s[i] === '}') {
+         if (stack.pop() !== '{') return false;
+       }
+    }
+    if (stack.length === 0) return true;
+    else return false;
+    
+};
