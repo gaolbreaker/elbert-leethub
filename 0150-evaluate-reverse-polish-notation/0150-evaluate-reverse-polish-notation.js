3c59136b8890c856b04eab071975555ac677d62e
Elbert Chan <elb.chan@gmail.com>
2022-12-01 00:43:06 -0800

    Time: 103 ms (70.99%), Space: 50.6 MB (10.21%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -9,13 +9,18 @@ var evalRPN = function(tokens) {
     "/": (a, b) => a / b >= 0 ? Math.floor(a / b) : Math.ceil(a / b), // "truncate toward 0"
     "*": (a, b) => a * b
   };
-  const token = tokens.pop();
-  const number = Number(token);
-  if (number || number === 0) return number;// base case: evaluate whether the token is a number and return it immediately if so
-  const operator = tokenToOperator[token];
-  const right = evalRPN(tokens); // recurse right side
-  const left = evalRPN(tokens); // recurse left side
-  return operator(left, right); 
+  
+  function helper() {
+    const token = tokens.pop();
+    const number = Number(token);
+    if (number || number === 0) return number;// base case: evaluate whether the token is a number and return it immediately if so
+    const operator = tokenToOperator[token];
+    const right = evalRPN(tokens); // recurse right side
+    const left = evalRPN(tokens); // recurse left side
+    return operator(left, right); 
+  }
+  
+  return helper();
 };
 
 // Reminder: 0 is falsy!
