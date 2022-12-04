cd2526faadb2b3b670dbc7ae8b8452426b653d4c
Elbert Chan <elb.chan@gmail.com>
2022-12-04 13:17:08 -0800

    Time: 405 ms (14.61%), Space: 63.6 MB (55.70%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
@@ -1,15 +1,17 @@
 class Trie {
+  
   constructor() {
     this.wordCount = 0;
     this.tree = new Node();
   }
   
   insert(word){
+    let i = 0;
+    let currNode;
     // Case of the first insertion:
     if (this.wordCount === 0) {
-      let i = 0;
       this.tree[word[i]] = new Node(word[i]);
-      let currNode = this.tree[word[i]];
+      currNode = this.tree[word[i]];
       while (i < word.length) {
         if (word[i + 1]) {
           currNode[word[i + 1]] = new Node(word[i + 1]);
@@ -21,13 +23,11 @@ class Trie {
       }
     }
     // Case of subsequent insertions:
-    if (this.wordCount > 0) {
-      // null -> 0 -> 1 -> 2
-      let i = 0;
+    else if (this.wordCount > 0) {
       if (this.tree[word[i]] == undefined) {
         this.tree[word[i]] = new Node(word[i]);
       }
-      let currNode = this.tree[word[i]];
+      currNode = this.tree[word[i]];
       while (i < word.length) {
         if (word[i + 1]) {
           if (currNode[word[i + 1]] === undefined) {
@@ -40,11 +40,7 @@ class Trie {
         i++;
       }
     }
-    
-    
-    // Console.log output
     this.wordCount++;
-    // console.log(JSON.stringify(this.tree));
   }
   
   search(word) {
@@ -54,7 +50,6 @@ class Trie {
       if (this.tree[word[i]] === undefined) return false;
       let currNode = this.tree[word[i]];
       while (i < word.length) {
-        // console.log(`i: ${i} word[i]: ${word[i]} currNode: ${currNode.val}`)
         if (word[i + 1]) {
           if (currNode[word[i + 1]] === undefined) return false;
         } else {
@@ -75,11 +70,9 @@ class Trie {
       if (this.tree[word[i]] === undefined) return false;
       let currNode = this.tree[word[i]];
       while (i < word.length) {
-        // console.log(`i: ${i} word[i]: ${word[i]} currNode: ${currNode.val}`)
         if (word[i + 1]) {
           if (currNode[word[i + 1]] === undefined) return false;
         } 
-      
         currNode = currNode[word[i + 1]];
         i++;
       }
@@ -88,14 +81,10 @@ class Trie {
   }
 }
 
+
 class Node {
   constructor(val) {
     this.val = val;
     this.end = false;
   }
 }
-
-// function Node(val) {
-//   this.val = val;
-//   this.end = false;
-// }
