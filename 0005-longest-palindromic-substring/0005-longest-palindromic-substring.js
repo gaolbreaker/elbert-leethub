250ba3c971deadd2421de2502f81982bf2b36c3e
Elbert Chan <elb.chan@gmail.com>
2022-12-11 23:11:14 -0800

    Time: 1071 ms (18.98%), Space: 49.1 MB (25.08%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  /*
+    I: string
+    O: longest palindromic substring
+    C: s.length >= 1, s.length <= 1000
+    E: s.length = 1
+    P:
+
+
+     babad
+     
+  */
+  let longest = '';
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < s.length; j++) {
+      let slice = s.slice(i, j + 1);
+      if (slice.length > longest.length && isPalindrome(slice)) longest = slice;
+    }
+  }
+  return longest;
+};
+
+var isPalindrome = function(s) {
+  let i = 0;
+  while (i < s.length / 2) {
+    if (s[i] !== s[s.length - 1 - i]) return false;
+    i++;
+  }
+  return true;
+}
