f6c4eb83cb5c238cbacbaa1d07cc28a7c4951f73
Elbert Chan <elb.chan@gmail.com>
2022-12-21 19:21:14 -0800

    Time: 86 ms (94.83%), Space: 51.3 MB (33.05%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  const freqMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!freqMap[nums[i]]) freqMap[nums[i]] = 1;
+    else return true;
+  }
+  return false;
+};
